5a10513a4a26768383f4a8630444f1540481f8e0
Adham Khatean <aekhatean@gmail.com>
2022-01-11 15:09:22 +0200

    Create products and orders handlers

---
+++ src/handlers/products_routes.ts
@@ -0,0 +1,47 @@
+import express, { Request, Response } from 'express';
+import jwt, { Secret } from 'jsonwebtoken';
+import { Product, Products } from '../models/products.model';
+const store = new Products();
+
+const index = async (_req: Request, res: Response) => {
+  const products = await store.index();
+  res.json(products);
+};
+
+const show = async (_req: Request, res: Response) => {
+  const product = await store.show(_req.body.id);
+  res.json(product);
+};
+
+const create = async (_req: Request, res: Response) => {
+  const product: Product = {
+    name: _req.body.name,
+    price: _req.body.price
+  };
+
+  try {
+    const newProduct = await store.create(product);
+    const token = jwt.sign(
+      { product: newProduct },
+      process.env.TOKEN_SECRET as unknown as Secret
+    );
+    res.json(token);
+  } catch (err) {
+    res.status(400);
+    res.json(product);
+  }
+};
+
+const destroy = async (_req: Request, res: Response) => {
+  const deleted = await store.delete(_req.body.id);
+  res.json(deleted);
+};
+
+const products_routes = (app: express.Application) => {
+  app.get('/products', index);
+  app.get('/products/:id', show);
+  app.post('/products', create);
+  app.delete('/products', destroy);
+};
+
+export default products_routes;
