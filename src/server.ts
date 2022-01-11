dc0a6ec57573d134f014c8d0efe558e8278fd73a
Adham Khatean <aekhatean@gmail.com>
2022-01-11 15:10:39 +0200

    Add users and products handlers to app server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 
+// Import endpoint handlers
+import users_routes from './handlers/users_routes';
+import products_routes from './handlers/products_routes';
+
 const app: express.Application = express();
 const address = 'http://127.0.0.1:3000';
 
@@ -9,6 +13,9 @@ app.use(bodyParser.json());
 app.get('/', function (_req: Request, res: Response) {
   res.send('Hello world!');
 });
+
+users_routes(app);
+products_routes(app);
 
 app.listen(3000, () => {
   console.log(`starting app on: ${address}`);
