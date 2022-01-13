2a7c843196370639e6b910a77226275dc6bf90b0
Adham Khatean <aekhatean@gmail.com>
2022-01-13 10:08:48 +0200

    Create create and show functions for orders.model

--- src/models/orders.model.ts
+++ src/models/orders.model.ts
@@ -1,23 +1,35 @@
 import Client from '../database';
 
 export type Order = {
-  orderId: number;
   userId: number;
   status: string;
 };
 
+export type OrderProduct = {
+  orderId: number;
+  productId: number;
+  quantity: string;
+};
 export class Orders {
-  async addProduct(
-    quantity: number,
-    orderId: string,
-    productId: string
-  ): Promise<Order> {
+  async show(id: number): Promise<Order> {
     try {
-      const sql =
-        'INSERT INTO order_products (quantity, order_id, product_id) VALUES($1, $2, $3) RETURNING *';
+      const conn = await Client.connect();
+      const sql = `SELECT * FROM orders WHERE id=($1)`;
+      const result = conn.query(sql, [id]);
+      conn.release();
+      return (await result).rows[0];
+    } catch (err) {
+      throw new Error(`Cannot connect to database: ${err}`);
+    }
+  }
+
+  async create(o: Order): Promise<Order> {
+    try {
+      const createOrderSQL =
+        'INSERT INTO orders (user_id, status) VALUES($1, $2) RETURNING *';
       const conn = await Client.connect();
 
-      const result = await conn.query(sql, [quantity, orderId, productId]);
+      const result = await conn.query(createOrderSQL, [o.userId, o.status]);
 
       const order = result.rows[0];
 
@@ -25,9 +37,29 @@ export class Orders {
 
       return order;
     } catch (err) {
-      throw new Error(
-        `Could not add product ${productId} to order ${orderId}: ${err}`
-      );
+      throw new Error(`Could not create order: ${err}`);
+    }
+  }
+
+  async addProduct(op: OrderProduct): Promise<Order> {
+    try {
+      const sql =
+        'INSERT INTO order_products (order_id, product_id, quantity) VALUES($1, $2, $3) RETURNING *';
+      const conn = await Client.connect();
+
+      const result = await conn.query(sql, [
+        op.orderId,
+        op.productId,
+        op.quantity
+      ]);
+
+      const order = result.rows[0];
+
+      conn.release();
+
+      return order;
+    } catch (err) {
+      throw new Error(`Could not add product to your order: ${err}`);
     }
   }
 }
