03b5b8d120b40f5cf3c7579c2e1b6af690c8f89d
Adham Khatean <aekhatean@gmail.com>
2022-01-13 20:21:24 +0200

    Create population migrations

---
+++ migrations/sqls/20220113175246-populate-tables-down.sql
@@ -0,0 +1,4 @@
+/* Replace with your SQL commands */
+DELETE FROM orders WHERE id = 1;
+DELETE FROM products WHERE id = 1;
+DELETE FROM users WHERE id = 1;
---
+++ migrations/sqls/20220113175246-populate-tables-up.sql
@@ -0,0 +1,4 @@
+/* Replace with your SQL commands */
+INSERT INTO users (first_name, last_name, username, password) VALUES('Adham', 'Khatean', 'aekhatean', '12345');
+INSERT INTO products (name, price) VALUES('Pencil', 2);
+INSERT INTO orders (user_id, status) VALUES(1, 'Pending');
